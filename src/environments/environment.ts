c3eba9ec4d699d3e5a8900a0cc0ac4c6c6832bdf
AngelAlonsoA <4ngeljesus@gmail.com>
2018-11-28 04:27:23 -0600

    Creado servicio AUTH, y el registro de usuarios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCsujRDeCSolMALwz5KjJdN8xjQy3F3Io",
+    authDomain: "ingsoftware-bc036.firebaseapp.com",
+    databaseURL: "https://ingsoftware-bc036.firebaseio.com",
+    projectId: "ingsoftware-bc036",
+    storageBucket: "ingsoftware-bc036.appspot.com",
+    messagingSenderId: "48755519083"
+  }
 };
 
 /*
